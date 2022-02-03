0f9259485f27a67964a4f7e352dea40b4ac4378d
Kataoka Yutaka <yutaka1985tolucky@docomo.ne.jp>
2022-02-04 03:41:47 +0900

    dotenvを追加

--- src/components/hooks/UseKy.js
+++ src/components/hooks/UseKy.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import ky from "ky";
+import dotenv from 'dotenv'
 
 //楽天APIのURL
 const BASE_URL =
--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,5 @@ module.exports = {
     extend: {},
   },
   plugins: [ 
-    require('@tailwindcss/custom-forms'),
   ],
 };
