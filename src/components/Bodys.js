65c80bd4b22ddf0d2fa1ad0068284aad7964b84a
Kataoka Yutaka <yutaka1985tolucky@docomo.ne.jp>
2022-02-04 16:43:05 +0900

    検索結果が０の時の処理を追加

--- src/components/Bodys.js
+++ src/components/Bodys.js
@@ -15,20 +15,36 @@ const Bodys = ({ books, title }) => {
         />
       </div>
     );
-  return (
-    <div>
-      <div className="flex">
-        <h1 className="text-blue-400">{title}の検索結果です</h1>
-        <h5 className="ml-auto">{books.hits}件ヒットしました</h5>
+  if (books.hits === 0) {
+    return (
+      <div>
+        <div className="flex">
+          <h1 className="text-blue-400">{title}の検索結果です</h1>
+          <h5 className="ml-auto">{books.hits}件ヒットしました</h5>
+        </div>
+        <hr />
+        <h1 className="mt-4 text-lg text-red-500">
+          本が見つかりませんでした
+          <br /> 別のキーワードで検索してください
+        </h1>
       </div>
-      <hr />
-      <div className="flex-wrap flex  mx-10 py-10 ">
-        {books.Items.map((book, index) => {
-          return <Body key={index} book={book}></Body>;
-        })}
+    );
+  } else {
+    return (
+      <div>
+        <div className="flex">
+          <h1 className="text-blue-400">{title}の検索結果です</h1>
+          <h5 className="ml-auto">{books.hits}件ヒットしました</h5>
+        </div>
+        <hr />
+        <div className="flex-wrap flex  mx-10 py-10 ">
+          {books.Items.map((book, index) => {
+            return <Body key={index} book={book}></Body>;
+          })}
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default Bodys;
