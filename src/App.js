24cff2bef4086de70e187e211b041b5915f16191
Kataoka Yutaka <yutaka1985tolucky@docomo.ne.jp>
2022-02-12 00:28:16 +0900

    router-domを追加

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import Home from "./components/Home";
 import { Route, BrowserRouter, Routes } from "react-router-dom";
 import NotFound from "./components/NotFound";
 import Footer from "./components/Footer";
-import BookDetail from "./components/BookDetail";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/bookDetail:sbin" element={<BookDetail />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
       <Footer />
--- src/components/Body.js
+++ src/components/Body.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 export default function Body ({ book }) {
 //titleが長すぎるとき途中で止める関数
@@ -15,11 +14,11 @@ export default function Body ({ book }) {
   return (
     <div className="py-4 pr-1">
       <div className="w-180 ">
-        <Link to='/bookDetail/:sbin' className="block w-32">
+        <a href={book.Item.itemUrl} className="block w-32">
           <img src={book.Item.largeImageUrl} />
-        </Link>
+        </a>
       </div>
-      <div className=''>
+      <div>
         <div>{convertitle(book.Item.title)}</div>
         <div>{convertitle(book.Item.author)}</div>
       </div>
